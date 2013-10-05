5a3d789e5f359f2d31d9a0cae609d2d7ce9f9165
ronys <ronys@users.sourceforge.net>
2013-10-05 19:35:11 +0100

    Write V4 keyblock

--- src/core/PWSfileV1V2.cpp
+++ src/core/PWSfileV1V2.cpp
@@ -18,9 +18,8 @@
 #include <sys/stat.h>
 
 PWSfileV1V2::PWSfileV1V2(const StringX &filename, RWmode mode, VERSION version)
-  : PWSfile(filename, mode)
+: PWSfile(filename, mode, version)
 {
-  m_curversion = version;
   m_IV = m_ipthing;
 }
 
